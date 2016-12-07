bb0fd2cb2a32aa86fef4d3990055a21475dd88c9
Daniel <Daniel@DESKTOP>
2016-12-07 11:52:06 +1100

    Support for parallel iterations

---
+++ base/__init__.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+"""
+Collection of utility operations that don't go anywhere else.
+"""
+
+import multiprocessing
+import numpy as np
+import tempfile
+import igraph
+import os
+
+
+def process_queue(func, q_in, q_out):
+    while True:
+        i, x = q_in.get()
+        if i is None:
+            break
+        q_out.put((i, func(x)))
+
+
+def parallel_map(func, iterator_to_parallelize, n_jobs):
+    q_in = multiprocessing.Queue(maxsize=1)
+    q_out = multiprocessing.Queue()
+
+    # Build the list of processes.
+    proc = [
+        multiprocessing.Process(target=process_queue, args=(func, q_in, q_out))
+        for _ in range(n_jobs)
+    ]
+
+    # Make each process a daemon to intercept calls, and start each one
+    for p in proc:
+        p.daemon = True
+        p.start()
+
+    # Send all the items in the iterator_to_parallelize to the job queue
+    sent = [q_in.put(obj=(i, x))
+            for i, x in enumerate(iterator_to_parallelize)]
+
+    # Initialise the Queue
+    [q_in.put((None, None)) for _ in range(n_jobs)]
+
+    # Get the results from the current Queue
+    accumulator = [q_out.get() for _ in range(len(sent))]
+
+    # Join the spawned processes so they can be released.
+    [p.join() for p in proc]
+
+    return [x for i, x in sorted(accumulator, key=lambda item: item[0])]
+
+
+def su_make_dir(path, mode=0o777):
+    if not path or os.path.exists(path):
+        print("{} already exists...".format(path))
+    else:
+        os.mkdir(path)
+        os.chmod(path, mode)
+
+
+def pretty_print_dict(dictionary, n_tabs=0):
+    for k in sorted(dictionary.keys()):
+        print('\t'*n_tabs + '{}:\t{}'.format(k, dictionary[k]))
+
+
+def create_seeds(size):
+    np.random.seed(42)
+    ii32 = np.iinfo(np.int32)
+    max_int = ii32.max
+    seeds = np.random.random_integers(low=0, high=max_int, size=size)
+    return seeds
+
+
+def chunks(ls, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(ls), n):
+        yield ls[i:i + n]
+
+
+def igraph_from_tuples(v_names):
+    tmp = tempfile.mktemp(suffix='.ncol', prefix='graph_', dir='tmp/')
+    fp = open(tmp, "w")
+    for (name_a, name_b) in v_names:
+        fp.write("{} {}\n".format(name_a, name_b))
+    fp.close()
+    # will store node names under the 'name' vertex attribute.
+    g = igraph.read(tmp, format='ncol', directed=False, names=True)
+    os.remove(tmp)
+    return g
+
+
+def igraph_vid_attr_table(igraph_g, attr):
+    vid_prop_lookup = {}
+    for v in igraph_g.vs:
+        data = v[attr]
+        vid_prop_lookup[v.index] = data
+    return vid_prop_lookup
+
+
+def write_cytoscape_attr_file(attr_name, attr_tuples, edge, fp):
+    if len(attr_tuples) == 0:
+        raise ValueError("attr_tuples must not be empty.")
+    if edge:
+        if len(attr_tuples[0]) != 3:
+            raise ValueError("Tuples must have length 3 for edge attr files.")
+        fp.write('Name\t{}\n'.format(attr_name.replace(" ", '-')))
+        fp.write('\n'.join(('{} (pp) {}\t{}'.format(a, b, attr)
+                            for (a, b, attr) in attr_tuples)))
+    else:
+        if len(attr_tuples[0]) != 2:
+            raise ValueError("Tuples must have length 2 for node attr files.")
+        fp.write('Name\t{}\n'.format(attr_name.replace(" ", '-')))
+        fp.write('\n'.join(('{}\t{}'.format(a, attr)
+                            for (a, attr) in attr_tuples)))
+    fp.close()
+
+
+def write_interactions(tuples, fp):
+    fp.write('source\ttarget\tinteraction\n')
+    fp.write('\n'.join(('{}\t{}\tpp'.format(a, b) for (a, b) in tuples)))
+    fp.close()
+
---
+++ model_selection/experiment.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+
+import numpy as np
+from sklearn.base import clone
+from sklearn.model_selection import StratifiedKFold, KFold
+from sklearn.externals.joblib import Parallel
+from sklearn.utils.validation import check_is_fitted
+
+from base import create_seeds
+from model_selection.sampling import IterativeStratifiedKFold
+
+
+class Bootstrap(object):
+    """
+    This is a class that performs KFold cross-validation to
+    perform a single KFoldExperiment a number of times.
+
+    Parameters
+    ----------
+    kfold_experiemnt : KFoldExperiment object
+        A KFoldExperiment object instantiated with the desired parameters
+        that will be used as a template for all iterations.
+
+    n_iter : int, optional
+        The number of iterations to compute.
+
+    n_jobs : int, optional
+        The number of parallel jobs to spawn to run iterations in parallel.
+
+    verbose : boolean, optional.
+        Set to true to print out intermediate messages from parallel and
+        joblib.
+
+    Attributes
+    ----------
+    experiments : List[kfold_experiemnt]
+        The fitted KFoldExperiment objects.
+    """
+
+    def __init__(self, kfold_experiemnt, n_iter, n_jobs=1, verbose=False):
+        seeds = create_seeds(n_iter)
+        self.n_iter = n_iter
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+        self.experiments = [kfold_experiemnt.clone(s) for s in seeds]
+
+    def _fit_kfold(self, X, y, i):
+        return self.experiments[i].fit(X, y)
+
+    def _scores(self, X, y, i, dispatch_func, score_funcs, thresholds, mean):
+        results = []
+        for (f, t), idx in enumerate(zip(score_funcs, thresholds)):
+            scores = getattr(self.experiments[i], dispatch_func)(
+                X, y, f, t, mean
+            )
+            results.append(scores)
+        return np.asarray(results)
+
+    def fit(self, X, y):
+        self.experiments = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
+                                    backend='multiprocessing')(
+            self._fit_kfold(X, y, i) for i in range(self.n_iter)
+        )
+        self.fitted_ = True
+        return self
+
+    def validation_scores(self, X, y, score_funcs, thresholds, mean=False):
+        check_is_fitted(self, '_fitted')
+        results = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
+                           backend='multiprocessing')(
+            self._scores(X, y, i, 'validation_score', score_funcs,
+                         thresholds, mean)
+            for i in range(self.n_iter)
+        )
+        return np.asarray(results)
+
+    def held_out_scores(self, X, y,  score_funcs, thresholds, mean=False):
+        check_is_fitted(self, '_fitted')
+        results = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
+                           backend='multiprocessing')(
+            self._scores(X, y, i, 'held_out_score', score_funcs,
+                         thresholds, mean)
+            for i in range(self.n_iter)
+        )
+        return np.asarray(results)
+
+
+class KFoldExperiment(object):
+    """
+    This is a class that performs KFold cross-validation to
+    collect numerous statistics over K folds.
+
+    Parameters
+    ----------
+    estimator : estimator object
+        An estimator object implementing `fit` and one of `decision_function`
+        or `predict_proba`.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross validation,
+          - integer, to specify the number of folds in a `(Stratified)KFold`,
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train, test splits.
+
+        For integer/None inputs, if the estimator is a classifier and ``y`` is
+        either binary or multiclass, :class:`StratifiedKFold` is used. In all
+        other cases, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validation strategies that can be used here.
+
+    shuffle : boolean, optional
+        Whether to shuffle each stratification of the data before splitting
+        into batches.
+
+    random_state : None, int or RandomState
+        When shuffle=True, pseudo-random number generator state used for
+        shuffling. If None, use default numpy RNG for shuffling.
+
+    Attributes
+    ----------
+    estimators_ : list of `n_classes` estimators
+        Estimators used for predictions on each fold.
+    """
+
+    def clone(self, random_state):
+        cv = clone(self.cv_)
+        cv.random_state = random_state
+        return KFoldExperiment(
+            estimator=clone(self.base_estimator_),
+            cv=cv,
+            shuffle=self.shuffle_,
+            random_state=random_state
+        )
+
+    def __init__(self, estimator, cv=3, shuffle=True, random_state=None):
+        self.base_estimator_ = clone(estimator)
+        self.estimators_ = []
+        self.shuffle_ = shuffle
+        self.random_state_ = random_state
+
+        if isinstance(cv, int):
+            self.cv_ = StratifiedKFold(cv, shuffle, random_state)
+        elif isinstance(cv, KFold):
+            self.cv_ = cv
+        elif isinstance(cv, StratifiedKFold):
+            self.cv_ = cv
+        elif isinstance(cv, IterativeStratifiedKFold):
+            self.cv_ = cv
+        else:
+            raise ValueError("Argument `cv` must be an integer or an "
+                             "instance of StratifiedKFold, KFold or "
+                             "IterativeStratifiedKFold.")
+        self.n_splits = self.cv_.n_splits
+
+    def fit(self, X, y):
+        """
+         Top-level function to train the estimators over each fold, without
+         performing validation.
+
+         :param X: numpy array X, shape (n_samples, n_features)
+         :param y: numpy array y, shape (n_samples, n_outputs)
+         :return: Self
+         """
+        if hasattr(self.cv_, 'split'):
+            self.cv_ = list(self.cv_.split(X, y))
+
+        for (train_idx, _) in self.cv_:
+            X_train = X[train_idx, ]
+            y_train = y[train_idx, ]
+
+            estimator = clone(self.base_estimator_)
+            estimator.fit(X_train, y_train)
+            self.estimators_.append(estimator)
+
+        self.fitted_ = True
+        return self
+
+    def validation_score(self, X, y, score_func, threshold=None, mean=False):
+        """
+        Prodivde the cross-validation scores on a the validation dataset.
+
+        :param X: numpy array X, shape (n_samples, n_features)
+        :param y: numpy array y, shape (n_samples, n_outputs)
+        :param score_func: function
+            Method with signature score_func(y, y_pred)
+        :param threshold: float, optional
+            If using probability predictons, the threshod represents the
+            positive prediction cut-off.
+        :param mean: booleam, optional
+            Return the averaged result accross the folds.
+
+        :return: List of scores as returned by score_func for each fold.
+        """
+        scores = []
+        check_is_fitted(self, '_fitted')
+        for (_, test_idx), estimator in zip(self.cv_, self.estimators_):
+            X_test = X[test_idx, ]
+            y_test = y[test_idx, ]
+
+            if hasattr(estimator, 'predict_proba'):
+                y_pred = estimator.predict_proba(X_test)
+                if threshold:
+                    y_pred = (y_pred >= threshold).astype(int)
+            else:
+                print("Warning: {} does not have a `predict_proba` "
+                      "implementation. Using `predict` "
+                      "instead.".format(type(estimator)))
+                y_pred = estimator.predict(X_test)
+
+            score = score_func(y_test, y_pred)
+            scores.append(score)
+
+        if mean:
+            return np.mean(scores)
+        return np.asarray(scores)
+
+    def held_out_score(self, X, y, score_func, threshold=None, mean=False):
+        """
+        Prodivde the cross-validation scores on a held out dataset.
+
+        :param X: numpy array X, shape (n_samples, n_features)
+        :param y: numpy array y, shape (n_samples, n_outputs)
+        :param score_func: function
+            Method with signature score_func(y, y_pred)
+        :param threshold: float, optional
+            If using probability predictons, the threshod represents the
+            positive prediction cut-off.
+        :param mean: booleam, optional
+            Return the averaged result accross the folds.
+
+        :return: List of scores as returned by score_func for each fold.
+        """
+        scores = []
+        check_is_fitted(self, '_fitted')
+        for estimator in self.estimators_:
+            if hasattr(estimator, 'predict_proba'):
+                y_pred = estimator.predict_proba(X)
+                if threshold:
+                    y_pred = (y_pred >= threshold).astype(int)
+            else:
+                print("Warning: {} does not have a `predict_proba` "
+                      "implementation. Using `predict` "
+                      "instead.".format(type(self.base_estimator_)))
+                y_pred = estimator.predict(X)
+
+            score = score_func(y, y_pred)
+            scores.append(score)
+
+        if mean:
+            return np.mean(scores)
+        return np.asarray(scores)
