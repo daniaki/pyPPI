aadf13268cfa2d3a274a65b6cf3c481bbd78bb3c
daniel <danielce90@gmail.com>
2017-03-19 16:55:30 +1100

    - Script bug fixes

--- pyPPI/base/__init__.py
+++ pyPPI/base/__init__.py
@@ -208,10 +208,14 @@ def parse_args(docopt_args):
     )
 
     # Numeric parsing
-    parsed['n_jobs'] = int(query_doctop_dict(docopt_args, '--n_jobs'))
-    parsed['n_splits'] = int(query_doctop_dict(docopt_args, '--n_splits'))
-    parsed['iterations'] = int(query_doctop_dict(docopt_args, '--iterations'))
-    parsed['threshold'] = float(query_doctop_dict(docopt_args, '--threshold'))
+    n_jobs = int(query_doctop_dict(docopt_args, '--n_jobs')) or 1
+    n_splits = int(query_doctop_dict(docopt_args, '--n_splits')) or 5
+    iterations = int(query_doctop_dict(docopt_args, '--iterations')) or 5
+    threshold = float(query_doctop_dict(docopt_args, '--threshold')) or 0.5
+    parsed['n_jobs'] = n_jobs
+    parsed['n_splits'] = n_splits
+    parsed['iterations'] = iterations
+    parsed['threshold'] = threshold
 
     # Input/Output parsing
     if query_doctop_dict(docopt_args, '--output'):
