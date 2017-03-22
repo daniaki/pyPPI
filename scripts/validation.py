3323b877edb2c3f0c7af2f9c6bbb25f5f1e11cb3
daniel <danielce90@gmail.com>
2017-03-22 21:26:07 +1100

    - Added logging messages

--- scripts/validation.py
+++ scripts/validation.py
@@ -132,11 +132,11 @@ if __name__ == '__main__':
     cv = IterativeStratifiedKFold(n_splits=n_splits, shuffle=True)
     kf = KFoldExperiment(
         estimator=clf, cv=cv, n_jobs=n_splits,
-        verbose=True, backend='multiprocessing'
+        verbose=verbose, backend='multiprocessing'
     )
     bootstrap = Bootstrap(
         kfold_experiemnt=kf, n_iter=n_iter, n_jobs=n_jobs,
-        verbose=True, backend='multiprocessing'
+        verbose=verbose, backend='multiprocessing'
     )
 
     # Fit the data
