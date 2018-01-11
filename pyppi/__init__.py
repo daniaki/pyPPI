65899b5348ebc274ece4b62aa6b8271e45556df7
Daniel Esposito <danielce90@gmail.com>
2018-01-11 13:51:57 +1100

    Bug fixes in parse_args

--- pyppi/__init__.py
+++ pyppi/__init__.py
@@ -1,3 +1,17 @@
+import logging
+
+logger = logging.getLogger("pyppi")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(handler)
+
+
 __all__ = [
     'base',
     'data',
