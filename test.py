2c65cdff74d1fc566327933ce534ac23fb976245
Daniel Esposito <danielce90@gmail.com>
2018-01-11 13:55:44 +1100

    Unit test driver

---
+++ test.py
@@ -0,0 +1,41 @@
+import unittest
+from pyppi.tests import (
+    test_database, test_features, test_uniprot,
+    test_db_models, test_managers, test_ontology
+)
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+
+    tests = loader.discover(start_dir='./', pattern="test_*.py")
+    unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_ontology.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_features.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_db_models.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_uniprot.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_managers.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_database.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_datamining_tools.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_generic.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_hprd.py")
+    # unittest.TextTestRunner().run(tests)
+
+    # tests = loader.discover(start_dir='./', pattern="test_kegg.py")
+    # unittest.TextTestRunner().run(tests)
