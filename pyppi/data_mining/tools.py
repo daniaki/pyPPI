efd98d2955aac5667265d1ffb58db4d4cb1b93ed
Daniel Esposito <danielce90@gmail.com>
2018-02-05 23:22:34 +1100

    Added fixes to dataframe methods to handle Null values more appropriately

--- pyppi/data_mining/tools.py
+++ pyppi/data_mining/tools.py
@@ -13,26 +13,49 @@ from collections import OrderedDict
 from itertools import product
 
 import pandas as pd
+import numpy as np
 from numpy import NaN
 
-from ..base import SOURCE, TARGET, LABEL
+from ..base import SOURCE, TARGET, LABEL, NULL_VALUES
 from ..base import PPI
 
 logger = logging.getLogger("pyppi")
 
 
-def _format_label(label):
-    if label is None or str(label) == str(NaN):
+def _null_to_none(value):
+    if str(value) in NULL_VALUES:
         return None
+    elif value in NULL_VALUES:
+        return None
+    else:
+        return value
+
+
+def _make_ppi_tuples(sources, targets):
+    sources = [_null_to_none(x) for x in sources]
+    targets = [_null_to_none(x) for x in targets]
+    ppis = [
+        tuple(sorted((str(a), str(b))))
+        for a, b in zip(sources, targets)
+    ]
+    return ppis
+
+
+def _split_label(label, sep=','):
+    return [l.strip() for l in str(_null_to_none(label)).strip().split(sep)]
+
+
+def _format_label(label):
+    if _null_to_none(label) is None:
+        return str(None).lower()
     return label.strip().lower().replace(" ", "-")
 
 
-def _format_labels(labels, sort_after_split=True, rejoin_after_split=False,
+def _format_labels(label_ls, sort_after_split=True, rejoin_after_split=False,
                    remove_duplicates_after_split=True, sep=','):
-    labels = [
-        [_format_label(l) for l in ls.strip().split(sep)]
-        for ls in labels
-    ]
+    labels = []
+    for ls in label_ls:
+        labels.append([_format_label(l) for l in _split_label(ls, sep)])
 
     if remove_duplicates_after_split:
         labels_ = []
@@ -85,6 +108,7 @@ def labels_from_interaction_frame(interactions):
     labels = _format_labels(
         df[LABEL], sort_after_split=True, remove_duplicates_after_split=True
     )
+    labels = [[_null_to_none(l) for l in ls] for ls in labels]
     return labels
 
 
@@ -102,11 +126,14 @@ def ppis_from_interaction_frame(interactions):
         List or Set of PPI objects.
     """
     df = interactions
-    ppis = [(a, b) for a, b in zip(df[SOURCE], df[TARGET])]
+    ppis = [
+        (_null_to_none(a), _null_to_none(b))
+        for a, b in zip(df[SOURCE], df[TARGET])
+    ]
     return ppis
 
 
-def make_interaction_frame(sources, targets, labels):
+def make_interaction_frame(sources, targets, labels, **additional_columns):
     """
     Wrapper to construct a non-directional PPI dataframe.
 
@@ -115,7 +142,7 @@ def make_interaction_frame(sources, targets, labels):
     :param labels: Edge label for the interaction.
     :return: DataFrame with SOURCE, TARGET and LABEL columns.
     """
-    ppis = [tuple(PPI(a, b)) for a, b in zip(sources, targets)]
+    ppis = _make_ppi_tuples(sources, targets)
     sources = [a for a, _ in ppis]
     targets = [b for _, b in ppis]
     labels = [_format_label(l) for l in labels]
@@ -124,7 +151,15 @@ def make_interaction_frame(sources, targets, labels):
         TARGET: targets,
         LABEL: labels
     }
-    return pd.DataFrame(data=interactions, columns=[SOURCE, TARGET, LABEL])
+
+    df_columns = [SOURCE, TARGET, LABEL]
+    for column_key in sorted(additional_columns.keys()):
+        interactions[column_key] = additional_columns[column_key]
+        df_columns.append(column_key)
+
+    return normalise_nan(
+        pd.DataFrame(data=interactions, columns=df_columns)
+    )
 
 
 def map_network_accessions(interactions, accession_map, drop_nan,
@@ -147,16 +182,28 @@ def map_network_accessions(interactions, accession_map, drop_nan,
     """
     ppis = []
     labels = []
-    for (a, b, l) in zip(interactions[SOURCE], interactions[TARGET],
-                         interactions[LABEL]):
+    df = interactions
+    base_columns = [SOURCE, TARGET, LABEL]
+    meta_columns = [col for col in df.columns if col not in base_columns]
+    metadata = {c: [] for c in meta_columns}
+
+    zipped = zip(
+        df[SOURCE], df[TARGET], df[LABEL], *[df[c] for c in meta_columns]
+    )
+    for a, b, l, *extra in zipped:
         sources = accession_map.get(a, [])
         targets = accession_map.get(b, [])
         for (s, t) in product(sources, targets):
-            ppis.append(tuple(PPI(s, t)))
+            ppis.append(tuple(sorted((str(s), str(t)))))
             labels.append(l)
+            for i, col in enumerate(meta_columns):
+                metadata[col].append(extra[i])
+
     sources = [a for a, _ in ppis]
     targets = [b for _, b in ppis]
-    new_interactions = make_interaction_frame(sources, targets, labels)
+    new_interactions = make_interaction_frame(
+        sources, targets, labels, **metadata
+    )
     new_interactions = process_interactions(
         interactions=new_interactions,
         drop_nan=drop_nan,
@@ -166,52 +213,87 @@ def map_network_accessions(interactions, accession_map, drop_nan,
         min_counts=min_counts,
         merge=merge
     )
-    return new_interactions
+    return normalise_nan(new_interactions)
 
 
-def remove_nan(interactions):
+def normalise_nan(interactions, replace=NULL_VALUES, replace_with=None):
     """
-    Drop interactions with missing source, target or labels as indicated
-    by np.NaN/None.
+    Replace values appearing in interactions defined in replace with None.
 
-    :param interactions: DataFrame with 'source', 'target' and 'label' columns.
-    :return: DataFrame with 'source', 'target' and 'label' columns.
+    Arguments
+    ---------
+    interactions: DataFrame with 'source', 'target' and 'label' columns.
+    replace: Tuple of values to replace.
+
+    Returns
+    -------
+    DataFrame with `null` values in replace, replaced with `None`
     """
-    df = interactions.replace(to_replace=str(None), value=NaN, inplace=False)
-    df.replace(to_replace=str(NaN), value=NaN, inplace=True)
-    df.dropna(axis=0, how='any', inplace=True)
-    df = df.reset_index(drop=True, inplace=False)
+    df = interactions
+    for value in replace:
+        df = df.replace(
+            to_replace=[value], value=[replace_with], inplace=False
+        )
     return df
 
 
-def remove_intersection(interactions, other):
+def remove_nan(interactions, subset=[SOURCE, TARGET, LABEL]):
     """
-    Remove any interaction from `interactions` appearing in `other` from
-    `interactions`.
+    Drop interactions with missing source, target or labels as indicated
+    by None/None.
+
+    :param interactions: DataFrame with 'source', 'target' and 'label' columns.
+    :return: DataFrame with 'source', 'target' and 'label' columns.
+    """
+    df = normalise_nan(interactions, replace_with=np.NaN)
+    df.dropna(axis=0, how='any', inplace=True, subset=subset)
+    selector = df.index.values
+    new_df = interactions.loc[selector, ].reset_index(drop=True, inplace=False)
+    return new_df
+
+
+def remove_intersection(interactions, other, use_label=True):
+    """
+    Remove any interaction from `interactions` appearing in `other`.
 
     :param interactions: DataFrame with 'source', 'target' and 'label' columns.
     :param other: DataFrame with 'source', 'target' and 'label' columns.
+    :param use_label: By default only look for (source, target, label) in the 
+                      `other` dataframe. Otherwise look for (source, target).
     :return: DataFrame with 'source', 'target' and 'label' columns.
     """
-    selector = set()
+    selector = []
     ppis_in_other = OrderedDict()
     other_ppis = list(zip(other[SOURCE], other[TARGET], other[LABEL]))
     for (source, target, label) in other_ppis:
-        a, b = sorted([source, target])
-        for (s, t, l) in product([a], [b], label.split(',')):
-            ppis_in_other[(s, t, l)] = True
+        a, b = sorted([str(source), str(target)])
+        if use_label:
+            for (s, t, l) in product([a], [b], _split_label(label)):
+                ppis_in_other[(s, t, l)] = True
+        else:
+            ppis_in_other[(a, b)] = True
 
     df = interactions.reset_index(drop=True, inplace=False)
     interactions_ppis = list(zip(df[SOURCE], df[TARGET], df[LABEL]))
     for i, (source, target, label) in enumerate(interactions_ppis):
-        a, b = sorted([source, target])
-        for (s, t, l) in product([a], [b], label.split(',')):
-            if ppis_in_other.get((s, t, l)) is None:
-                selector.add(i)
+        a, b = sorted([str(source), str(target)])
+        if use_label:
+            for (s, t, l) in product([a], [b], _split_label(label)):
+                if ppis_in_other.get((s, t, l)) is None:
+                    selector.append(True)
+                else:
+                    selector.append(False)
+        else:
+            if ppis_in_other.get((a, b)) is None:
+                selector.append(True)
+            else:
+                selector.append(False)
 
-    df = interactions.loc[selector, ]
-    df = df.reset_index(drop=True, inplace=False)
-    return df
+    selector = np.asarray(selector)
+    df = interactions.loc[selector, ].reset_index(drop=True, inplace=False)
+    removed = interactions.loc[~selector, ].reset_index(
+        drop=True, inplace=False)
+    return df, removed
 
 
 def remove_labels(interactions, labels_to_exclude):
@@ -222,8 +304,15 @@ def remove_labels(interactions, labels_to_exclude):
     :param labels_to_exclude: string list of subtypes to exclude.
     :return: DataFrame with 'source', 'target' and 'label' columns.
     """
-    labels = interactions.label.values
-    selector = [(l not in labels_to_exclude) for l in labels]
+    def exclude(l):
+        if str(l) == str(np.NaN):
+            return str(np.NaN) in labels_to_exclude
+        else:
+            return l in labels_to_exclude
+    selector = [
+        not exclude(l)
+        for l in interactions[LABEL].values
+    ]
     df = interactions.loc[selector, ]
     df = df.reset_index(drop=True, inplace=False)
     return df
@@ -232,7 +321,7 @@ def remove_labels(interactions, labels_to_exclude):
 def remove_min_counts(interactions, min_count):
     """
     Remove all PPIs with labels that have overall count less
-    than some threshold.
+    than some threshold. Ignores labels that are NaN/null valued.
 
     :param interactions: DataFrame with 'source', 'target' and 'label' columns.
     :param min_count: Minimum count threshold to keep label.
@@ -241,7 +330,7 @@ def remove_min_counts(interactions, min_count):
     counts = Counter(interactions.label.values)
     labels_to_exclude = set([k for k, v in counts.items() if v < min_count])
     df = remove_labels(interactions, labels_to_exclude)
-    df = df.reset_index(drop=True, inplace=False)
+    df.reset_index(drop=True, inplace=True)
     return df
 
 
@@ -269,22 +358,58 @@ def merge_labels(interactions):
     :return: DataFrame with 'source', 'target' and 'label' columns.
     """
     df = interactions.reset_index(drop=True, inplace=False)
+    base_columns = [SOURCE, TARGET, LABEL]
+    extra_columns = [col for col in df.columns if col not in base_columns]
+
     merged_ppis = OrderedDict()
-    ppis = [tuple(PPI(s, t)) for (s, t) in zip(df.source, df.target)]
+    ppis = [
+        tuple(sorted((str(s), str(t))))
+        for (s, t) in zip(df[SOURCE], df[TARGET])
+    ]
+    zipped = zip(
+        ppis, df[LABEL], *[df[c] for c in extra_columns]
+    )
 
-    for (s, t), label in zip(ppis, interactions.label):
-        if (s, t) in merged_ppis:
-            merged_ppis[(s, t)].append(_format_label(label))
-        else:
-            merged_ppis[(s, t)] = [_format_label(label)]
+    for (s, t), label, *extra in zipped:
+        if merged_ppis.get((s, t)) is None:
+            merged_ppis[(s, t)] = {k: [] for k in extra_columns + [LABEL]}
+        # Some labels might be merged already, so split them first before
+        # formatting.
+        merged_ppis[(s, t)][LABEL].extend(
+            [
+                _format_label(l) for l in _split_label(label)
+                if _format_label(l) not in NULL_VALUES
+            ]
+        )
+        # Same as above, some additional data may have already been merged.
+        for i, column in enumerate(extra_columns):
+            merged_ppis[(s, t)][column].extend(
+                [
+                    e.strip() for e in _split_label(extra[i])
+                    if e.strip() not in NULL_VALUES
+                ]
+            )
 
+    # Format the labels by set, sorted and then comma delimiting.
     sources = [ppi[0] for ppi in merged_ppis.keys()]
     targets = [ppi[1] for ppi in merged_ppis.keys()]
     labels = [
-        ','.join(list(sorted(set(_format_label(l) for l in ls))))
-        for ls in list(merged_ppis.values())
+        ','.join(list(sorted(set(_format_label(l) for l in ls)))) or None
+        for ls in [row[LABEL] for row in merged_ppis.values()]
     ]
-    interactions = make_interaction_frame(sources, targets, labels)
+
+    # Format the additonal columns by set and sorting.
+    additional = {c: [] for c in extra_columns}
+    for column in extra_columns:
+        data = [
+            ','.join(list(sorted(set(d.strip() for d in ls)))) or None
+            for ls in [row[column] for row in merged_ppis.values()]
+        ]
+        additional[column] = data
+
+    interactions = make_interaction_frame(
+        sources, targets, labels, **additional
+    )
     return interactions
 
 
@@ -297,23 +422,61 @@ def remove_duplicates(interactions):
     """
     df = interactions
     merged_ppis = OrderedDict()
-    ppis = [tuple(PPI(s, t)) for (s, t) in zip(df.source, df.target)]
-    merged = any([len(l.split(',')) > 1 for l in df[LABEL]])
+    ppis = [
+        tuple(sorted((str(s), str(t))))
+        for (s, t) in zip(df[SOURCE], df[TARGET])
+    ]
+    merged = any([len(_split_label(l)) > 1 for l in df[LABEL]])
+    base_columns = [SOURCE, TARGET, LABEL]
+    extra_columns = [col for col in df.columns if col not in base_columns]
+    zipped = zip(
+        ppis, df[LABEL], *[df[c] for c in extra_columns]
+    )
 
     if merged:
-        assert len(ppis) == len(df.label.values)
-        for (s, t), label in zip(ppis, df.label):
-            for l in set(label.split(',')):
-                merged_ppis[(s, t, l)] = 1
+        assert len(ppis) == len(df[LABEL].values)
+        for (s, t), label, *extra in zipped:
+            for l in set(_split_label(label)):
+                if merged_ppis.get((s, t, l)) is None:
+                    merged_ppis[(s, t, l)] = {c: [] for c in extra_columns}
+                for i, column in enumerate(extra_columns):
+                    merged_ppis[(s, t, l)][column].extend(
+                        [
+                            e.strip() for e in _split_label(extra[i])
+                            if e.strip() not in NULL_VALUES
+                        ]
+                    )
     else:
         assert len(ppis) == len(df.label.values)
-        for (s, t), label in zip(ppis, df.label):
-            merged_ppis[(s, t, label)] = 1
+        for (s, t), label, *extra in zipped:
+            if label in NULL_VALUES or str(label) in NULL_VALUES:
+                label = str(None)
+            if merged_ppis.get((s, t, label)) is None:
+                merged_ppis[(s, t, label)] = {c: [] for c in extra_columns}
+            for i, column in enumerate(extra_columns):
+                merged_ppis[(s, t, label)][column].extend(
+                    [
+                        e.strip() for e in _split_label(extra[i])
+                        if e.strip() not in NULL_VALUES
+                    ]
+                )
 
     sources = [ppi[0] for ppi in merged_ppis.keys()]
     targets = [ppi[1] for ppi in merged_ppis.keys()]
     labels = [ppi[2] for ppi in merged_ppis.keys()]
-    interactions = make_interaction_frame(sources, targets, labels)
+
+    # Format the additonal columns by set and sorting.
+    additional = {c: [] for c in extra_columns}
+    for column in extra_columns:
+        data = [
+            ','.join(list(sorted(set(d.strip() for d in ls)))) or None
+            for ls in [row[column] for row in merged_ppis.values()]
+        ]
+        additional[column] = data
+
+    interactions = make_interaction_frame(
+        sources, targets, labels, **additional
+    )
 
     if merged:
         interactions = merge_labels(interactions)
@@ -336,17 +499,27 @@ def process_interactions(interactions, drop_nan, allow_self_edges,
                   different labels into the same entry.
     :return: DataFrame with 'source', 'target' and 'label' columns.
     """
-    interactions = interactions.reset_index(drop=True, inplace=False)
+    interactions = normalise_nan(
+        interactions.reset_index(drop=True, inplace=False)
+    )
+    if drop_nan == 'default':
+        drop_nan = [SOURCE, TARGET, LABEL]
+    elif (not isinstance(drop_nan, list)) and (not drop_nan is None):
+        raise TypeError(
+            "`drop_nan` must be either a list of columns to search for None "
+            "values over, None to bypass this process or 'default' to specify "
+            "the default columns."
+        )
 
     if drop_nan:
-        interactions = remove_nan(interactions)
+        interactions = remove_nan(interactions, subset=drop_nan)
     if not allow_self_edges:
         interactions = remove_self_edges(interactions)
     if not allow_duplicates:
         interactions = remove_duplicates(interactions)
     if exclude_labels:
         interactions = remove_labels(interactions, exclude_labels)
-    if min_counts:  # This must come before merge.
+    if min_counts:  # This must come before merge as merge will concat labels.
         interactions = remove_min_counts(interactions, min_count=min_counts)
     if merge:
         interactions = merge_labels(interactions)
