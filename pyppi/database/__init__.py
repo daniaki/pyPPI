60f3c51a86a17f5a1b09231d65157a8a4177688e
Daniel Esposito <danielce90@gmail.com>
2018-02-05 23:26:24 +1100

    Fixed bug where starting a session would not create all tables; Re-wrote the delete method to delete rows instead of dropping tables.

--- pyppi/database/__init__.py
+++ pyppi/database/__init__.py
@@ -19,8 +19,13 @@ Base = declarative_base()
 
 
 def __init_database(engine):
-    from .models import Protein, Interaction
+    from .models import (
+        Protein, Interaction, Pubmed, Psimi,
+        psimi_interactions, pmid_interactions
+    )
     Base.metadata.create_all(bind=engine)
+    psimi_interactions.create(bind=engine, checkfirst=True)
+    pmid_interactions.create(bind=engine, checkfirst=True)
 
 
 try:
@@ -35,6 +40,7 @@ except:
 
 @contextmanager
 def begin_transaction(db_path=None, echo=False):
+    session = None
     try:
         if db_path is not None:
             engine = create_engine("sqlite:///" + os.path.normpath(db_path))
@@ -44,7 +50,61 @@ def begin_transaction(db_path=None, echo=False):
 
         session = Session(bind=engine)
         yield session
+        session.commit()
         session.close()
+    except:
+        if session is not None:
+            session.rollback()
+        raise
 
+
+def make_session(db_path=None, echo=False):
+    try:
+        if db_path is not None and db_path != default_db_path:
+            engine = create_engine("sqlite:///" + os.path.normpath(db_path))
+            __init_database(engine)
+        else:
+            engine = ENGINE
+        session = Session(bind=engine)
+        return session
     except:
         raise
+
+
+def delete_database(session=None, db_path=None):
+    from ..database.models import (
+        Protein, Interaction, Pubmed, Psimi,
+        psimi_interactions, pmid_interactions
+    )
+    if db_path is None:
+        db_path = default_db_path
+
+    if session is None:
+        with begin_transaction(db_path=db_path) as session:
+            session.query(Protein).delete()
+            session.query(Interaction).delete()
+            session.query(Pubmed).delete()
+            session.query(Psimi).delete()
+
+            session.query(psimi_interactions).delete(synchronize_session=False)
+            session.query(pmid_interactions).delete(synchronize_session=False)
+
+            try:
+                session.commit()
+            except:
+                session.rollback()
+                raise
+    else:
+        session.query(Protein).delete()
+        session.query(Interaction).delete()
+        session.query(Pubmed).delete()
+        session.query(Psimi).delete()
+
+        session.query(psimi_interactions).delete(synchronize_session=False)
+        session.query(pmid_interactions).delete(synchronize_session=False)
+
+        try:
+            session.commit()
+        except:
+            session.rollback()
+            raise
