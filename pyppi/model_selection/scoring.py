4e4847a410a548e3b03b23ef5458eb6e6a4994f9
Daniel Esposito <danielce90@gmail.com>
2018-02-16 08:12:59 +1100

    Positive hamming score metric

--- pyppi/model_selection/scoring.py
+++ pyppi/model_selection/scoring.py
@@ -7,7 +7,7 @@ and a class for housing statistics which wraps around a pandas dataframe.
 
 import numpy as np
 
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, hamming_loss
 
 
 def specificity(y_true, y_pred):
@@ -20,6 +20,32 @@ def fdr_score(y_true, y_pred):
     if (tp + fp) == 0:
         return 0.0
     return fp / (tp + fp)
+
+
+def positive_label_hammming_loss(y_true, y_score):
+    """
+    This is a relaxed version of the Hamming Loss function.
+    It computes the hamming loss over the positive labels appearing in
+    `y_true` only. This may be desirable when you are most interested in
+    penalising false negatives, or where false positives may represent
+    latent labels not seen during training and are not necessarily incorrect.
+    """
+    selectors = [np.where(row == 1)[0] for row in y_true]
+    y_true = [
+        row[selector]
+        for row, selector in zip(y_true, selectors)
+        if len(selector)
+    ]
+    y_score = [
+        row[selector]
+        for row, selector in zip(y_score, selectors)
+        if len(selector)
+    ]
+    losses = [
+        hamming_loss(row_true, row_score)
+        for row_true, row_score in zip(y_true, y_score)
+    ]
+    return sum(losses) / len(selectors)
 
 
 class MultilabelScorer(object):
