49ba7a2826ab06d42f6852fe10213a33296b0f3c
daniel <danielce90@gmail.com>
2018-02-20 10:12:48 +1100

    shell change

--- scripts/run_pred_exp.sh
+++ scripts/run_pred_exp.sh
@@ -7,7 +7,7 @@
 # python predict_ppis.py --cc --bp --mf --n_jobs=4 --n_iterations=30 --n_splits=3
 
 echo "Running isolated InterPro features experiment"
-python predict_ppis.py --interpro --n_jobs=4 --n_iterations=1 --n_splits=1
+python predict_ppis.py --interpro --n_jobs=4 --n_iterations=30 --n_splits=1
 
 echo "Running isolated Pfam features experiment"
 python predict_ppis.py --pfam --n_jobs=4 --n_iterations=30 --n_splits=3
