aa63d02b4eb51a5ff1560e902e9e9fdc8a81be51
daniaki <danielce90@gmail.com>
2019-08-11 18:49:23 +1000

    added stub methods

--- src/database/utilities.py
+++ src/database/utilities.py
@@ -1,13 +1,113 @@
-from typing import Iterable, List
+from logging import getLogger
+from typing import Iterable, List, Dict
 
-from ..parsers.types import Interaction
+from ..clients import uniprot
+from ..parsers.types import InteractionData, GeneOntologyTermData
+from ..settings import DATABASE, LOGGER_NAME
 
 from . import models
 
 
-def create_interactions(interactions: Iterable[Interaction]) -> None:
+logger = getLogger(LOGGER_NAME)
+
+
+@DATABASE.atomic()
+def create_go_terms(terms: Iterable[GeneOntologyTermData]):
     pass
 
 
-def create_proteins(proteins: Iterable[str]) -> None:
+@DATABASE.atomic()
+def create_pfam_terms(terms):
     pass
+
+
+@DATABASE.atomic()
+def create_interpro_terms(terms):
+    pass
+
+
+@DATABASE.atomic()
+def create_keywords(keywords):
+    pass
+
+
+@DATABASE.atomic()
+def create_identifiers(identifiers, model):
+    pass
+
+
+@DATABASE.atomic()
+def create_gene_symbols(symbols):
+    pass
+
+
+@DATABASE.atomic()
+def create_experiment_types(types):
+    pass
+
+
+@DATABASE.atomic()
+def create_interactions(interactions: Iterable[InteractionData]) -> None:
+    # Collect all UniProt identifiers to pass to create_proteins.
+    # create_proteins will download the uniprot entries and create the
+    # co-responding the database models
+    proteins: List[str] = []
+    interaction: InteractionData
+    for interaction in interactions:
+        if interaction.source:
+            proteins.append(interaction.source)
+        if interaction.target:
+            proteins.append(interaction.target)
+
+    proteins: Dict[str, models.Protein] = create_proteins(proteins)
+
+    for interaction in interactions:
+        # Skip interactions for which no uniprot entries could be found.
+        if proteins.get(interaction.source, None):
+            logger.warning(
+                f"Skipping interaction {interaction}. No source found "
+                f"on UniProt."
+            )
+            continue
+        if proteins.get(interaction.target, None):
+            logger.warning(
+                f"Skipping interaction {interaction}. No target found "
+                f"on UniProt."
+            )
+            continue
+
+        models.Interaction.update_or_create(
+            source=proteins[interaction.source],
+            target=proteins[interaction.target],
+            organism=interaction.organism,
+            labels=interaction.labels,
+            psimi_ids=interaction.psimi_ids,
+            pubmed_ids=interaction.pubmed_ids,
+            experiment_types=interaction.experiment_types,
+            databases=interaction.databases,
+        )
+
+
+@DATABASE.atomic()
+def create_proteins(proteins: Iterable[str]) -> Dict[str, models.Protein]:
+    client = uniprot.UniprotClient()
+    proteins: List[uniprot.UniprotEntry] = client.get_entries(proteins)
+
+    # go_terms = []
+    # pfam_terms = []
+    # interpro_terms = []
+    # keywords = []
+
+    # protein: uniprot.UniprotEntry
+    # for protein in proteins:
+    #     models.GeneOntologyTerm()
+
+    # result: Dict[str, models.Protein] = {}
+    # for protein in proteins:
+    #     models.Protein.update_or_create(
+    #         identifier=protein.accessions[0],
+    #         sequence=protein.sequence,
+    #         organism=protein.taxonomy,
+    #         aliases=protein.accessions,
+    #         go=[],
+    #     )
