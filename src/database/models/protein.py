7065de312762d4b9080a9d5bc761ae0a5ac8a868
daniaki <danielce90@gmail.com>
2019-09-07 19:58:04 +1000

    Refactored Protein into Protein and ProteinData

--- src/database/models/protein.py
+++ src/database/models/protein.py
@@ -1,5 +1,5 @@
 import peewee
-from typing import Iterable
+from typing import Iterable, Optional
 
 from ...constants import GeneOntologyCategory
 from ...utilities import is_null
@@ -15,17 +15,10 @@ from .metadata import (
 )
 
 
-__all__ = ["Protein"]
+__all__ = ["Protein", "ProteinData"]
 
 
-class Protein(BaseModel):
-    identifier = peewee.ForeignKeyField(
-        model=UniprotIdentifier,
-        null=False,
-        default=None,
-        unique=True,
-        backref="proteins",
-    )
+class ProteinData(BaseModel):
     organism = peewee.IntegerField(
         null=False, default=None, help_text="NCBI organism code. Eg 9606."
     )
@@ -35,55 +28,78 @@ class Protein(BaseModel):
         help_text="The protein sequence in single letter format.",
     )
     reviewed = peewee.BooleanField(
-        null=True,
+        null=False,
         default=None,
         help_text="Protein has been reviewed (Swiss-prot).",
     )
+    version = peewee.CharField(
+        null=False,
+        default=None,
+        max_length=16,
+        help_text="UniProt record version.",
+    )
 
     # --- M2M --- #
-    genes = peewee.ManyToManyField(model=GeneSymbol, backref="proteins")
-    aliases = peewee.ManyToManyField(
-        model=UniprotIdentifier, backref="alias_proteins"
+    genes = peewee.ManyToManyField(
+        model=GeneSymbol, backref="protein_data_set"
+    )
+    identifiers = peewee.ManyToManyField(
+        model=UniprotIdentifier, backref="protein_data_set"
     )
     go_annotations = peewee.ManyToManyField(
-        model=GeneOntologyTerm, backref="proteins"
+        model=GeneOntologyTerm, backref="protein_data_set"
     )
     interpro_annotations = peewee.ManyToManyField(
-        model=InterproTerm, backref="proteins"
+        model=InterproTerm, backref="protein_data_set"
     )
     pfam_annotations = peewee.ManyToManyField(
-        model=PfamTerm, backref="proteins"
+        model=PfamTerm, backref="protein_data_set"
     )
-    keywords = peewee.ManyToManyField(model=Keyword, backref="proteins")
-
-    def __str__(self):
-        try:
-            # Accessing the identifier field results in a database lookup.
-            return str(self.identifier)
-        except peewee.DoesNotExist:
-            return str(None)
+    keywords = peewee.ManyToManyField(
+        model=Keyword, backref="protein_data_set"
+    )
 
     def _select_go(self, category: str) -> peewee.ModelSelect:
         return (
-            GeneOntologyTerm.select()
-            .join(Protein.go_annotations.get_through_model())
-            .join(Protein)
-            .where(Protein.id == self.id)
-            .select()
+            GeneOntologyTerm.select(ProteinData, GeneOntologyTerm)
+            .join(ProteinData.go_annotations.get_through_model())
+            .join(ProteinData)
+            .where(ProteinData.id == self.id)
             .where(GeneOntologyTerm.category == category)
+            .switch(ProteinData)
         )
 
     @classmethod
     def get_by_identifier(
-        cls, identifiers: Iterable[str]
+        cls,
+        identifiers: Iterable[str],
+        query: Optional[peewee.ModelSelect] = None,
     ) -> peewee.ModelSelect:
+        """
+        Search protein data rows by UniProt identifier. This Will return all 
+        rows that have primary or alias identifiers matching any given in 
+        `identifiers`.
+        
+        Parameters
+        ----------
+        identifiers : Iterable[str]
+            Uniprot accession strings.
+        
+        query : Optional[ModelSelect], optional
+            Query to filter. Defaults to all rows.
+        
+        Returns
+        -------
+        peewee.ModelSelect
+        """
+        query = query or cls.all()
+        unique = set(i.lower() for i in identifiers)
         return (
-            cls.select()
+            query.select(cls, UniprotIdentifier)
+            .join(cls.identifiers.get_through_model())
             .join(UniprotIdentifier)
-            .where(
-                peewee.fn.Upper(UniprotIdentifier.identifier)
-                << set(i.upper() for i in identifiers)
-            )
+            .where(peewee.fn.Lower(UniprotIdentifier.identifier).in_(unique))
+            .switch(cls)
         )
 
     @property
@@ -103,3 +119,54 @@ class Protein(BaseModel):
             None if is_null(self.sequence) else self.sequence.strip().upper()
         )
         return super().format_for_save()
+
+
+class Protein(BaseModel):
+    identifier = peewee.ForeignKeyField(
+        model=UniprotIdentifier,
+        null=False,
+        default=None,
+        unique=True,
+        backref="proteins",
+    )
+    data = peewee.ForeignKeyField(
+        model=ProteinData, null=False, default=None, backref="proteins"
+    )
+
+    def __str__(self):
+        try:
+            # Accessing the identifier field results in a database lookup.
+            return str(self.identifier)
+        except peewee.DoesNotExist:
+            return str(None)
+
+    @classmethod
+    def get_by_identifier(
+        cls,
+        identifiers: Iterable[str],
+        query: Optional[peewee.ModelSelect] = None,
+    ) -> peewee.ModelSelect:
+        """
+        Search protein rows by UniProt identifier. This Will return all 
+        rows that have an identifier matching any given in `identifiers`.
+        
+        Parameters
+        ----------
+        identifiers : Iterable[str]
+            Uniprot accession strings.
+        
+        query : Optional[ModelSelect], optional
+            Query to filter. Defaults to all rows.
+        
+        Returns
+        -------
+        peewee.ModelSelect
+        """
+        query = query or cls.all()
+        unique = set(i.lower() for i in identifiers)
+        return (
+            query.select(cls, UniprotIdentifier)
+            .join(UniprotIdentifier)
+            .where(peewee.fn.Lower(UniprotIdentifier.identifier).in_(unique))
+            .switch(cls)
+        )
